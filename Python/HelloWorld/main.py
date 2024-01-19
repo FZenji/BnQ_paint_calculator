dbc03f3114e0d5a7109215535d68937174cea4db
henmt <henmtol@gmail.com>
2024-01-19 10:35:18 +0000

    Refactor repo

---
+++ Python/HelloWorld/main.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    name = "Henry"
+    age = 21
+    print(f"Hello {name}, I'm {str(age)}")
+
+
+
+    #print(f"Hello {name},\nI can solve a Rubik's Cube\nI know Pi to 500 digits\nI got stung by a jellyfish")
